8f7307e0a23803ff30be038bcbb7d65a1675c6ed
luiz-flamorim <luiz.flamorim@gmail.com>
2021-03-29 22:50:08 +0100

    genre with clours working

--- js/newdots.js
+++ js/newdots.js
@@ -1,6 +1,6 @@
 const height = window.innerHeight
-const width = 1000
-const radius = 4
+const width = window.innerWidth
+const radius = 5
 const step = radius * 2
 const theta = Math.PI * (3 - Math.sqrt(5))
 
@@ -33,26 +33,37 @@ function createChart(d, width, height, radius, step, theta) {
         return obj
     })
 
-
     const svg = d3.select('#dots')
         .attr("viewBox", [0, 0, width, height]);
 
     const g = svg.append("g")
         .attr("class", "circles");
 
-    g.append("style").text(`
-    .circles {
-        stroke: transparent;
-        stroke-width: 1.5px;
-    }
-    .circles circle:hover {
-        stroke: black;
-    }
-    `);
 
-    // let colour = d3.scaleOrdinal()
-    //     .domain(m.genre)
-    //     .range(['black', 'blue', 'red'])
+    let colourRange = (['#b9506eff',
+        '#E7442E',
+        '#111418',
+        '#3747D2',
+        '#1c2478ff',
+        '#4c54a9ff',
+        '#523883ff',
+        '#342a51ff',
+        '#1c3353',
+        '#332327ff',
+        '#80312aff',
+        '#e7442eff',
+        '#fff',
+        '#643259'
+    ])
+
+    let uniqueGenres = new Set(m.map(d => d.genre))
+    console.log(uniqueGenres)
+
+    let br = document.createElement("br")
+
+    let colour = d3.scaleOrdinal()
+        .domain(uniqueGenres)
+        .range(colourRange)
 
     g.selectAll("circle")
         .data(m, d => d.id)
@@ -60,17 +71,10 @@ function createChart(d, width, height, radius, step, theta) {
         .attr("cx", d => d.x)
         .attr("cy", d => d.y)
         .attr("r", radius)
-        // .attr("fill", (d, i) => d3.interpolateRainbow(i / 360))
-        // .attr("fill", m => console.log(m.genre))
-        // .attr("fill", d => //define the scale of genres and pass to d.genre)
-
-        .attr("fill", m => d3.scaleOrdinal(m.genre)
-            .domain(m.genre)
-            .range(['black', 'blue', 'red']))
-
+        .attr("fill", d => colour(d.genre))
         .on("mousedown", mousedowned)
         .append("title")
-        .text(d => `${d.title}`)
+        .text(d => d.title + ((d.chapter == "null") ? "" : " | " + d.chapter))
 
     svg.call(d3.zoom()
         .extent([
@@ -81,13 +85,11 @@ function createChart(d, width, height, radius, step, theta) {
         .on("zoom", zoomed));
 
     function mousedowned(event, d) {
-        console.log(d)
-
         d3.select(this).transition()
-            .attr("fill", "black")
-            .attr("r", radius * 2)
+            .attr("fill", "white")
+            .attr("r", radius * 3)
             .transition()
-            .attr("fill", d3.interpolateRainbow(d.id / 360))
+            .attr("fill", d => colour(d.genre))
             .attr("r", radius)
     }
 
@@ -97,4 +99,29 @@ function createChart(d, width, height, radius, step, theta) {
         g.attr("transform", transform);
     }
     return svg.node();
+}
+
+
+function cardBuilder(data) {
+
+    // let card = 
+
+
+    // let wrapper = document.createElement('div');
+    // wrapper.setAttribute("class", "col-sm-3 col-md-4 col-lg-2 h-100");
+    // wrapper.setAttribute("style", "card");
+    // wrapper.setAttribute("id", id);
+    // filmsContainer.appendChild(wrapper);
+
+    // let filmPoster = document.createElement('img');
+    // filmPoster.setAttribute("src", poster);
+    // filmPoster.setAttribute("class", "card-img-top img-fluid");
+    // filmPoster.addEventListener('click', (event) => {
+    //     filmDetail(id, title, year, overview, poster);
+    // })
+    // wrapper.appendChild(filmPoster);
+
+    // let cardInfo = document.createElement('div');
+    // cardInfo.setAttribute("class", "card-body flex-column");
+    // wrapper.appendChild(cardInfo);
 }
