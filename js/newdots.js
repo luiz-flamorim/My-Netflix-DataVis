cba45c54ea2046cf6f6b757657715fb734faf8c1
luiz-flamorim <luiz.flamorim@gmail.com>
2021-12-15 12:15:52 -0300

    basically finished the Phyllotax

--- js/newdots.js
+++ js/newdots.js
@@ -1,11 +1,15 @@
 // TO DO
-// mousetip - tippy
+// size of the overall circle chart(phyllotax) to vary: if the user has more films, the bubbles needs to be smaller.
+// legend: centre and make it responsive - viewbox is not working because the width/ height of the div is different from window.inner...
+// what happens when the user click on a film that doens't have information? address this with a different pop up (?)
+// grid diagram: reposition
 
-const height = window.innerHeight
+const height = window.innerHeight * 0.8
 const width = window.innerWidth
 const radius = 4
 const step = radius * 2
 const theta = Math.PI * (3 - Math.sqrt(5))
+let mapRadius = new Map()
 
 d3.json('js/results.json')
     .then(data => {
@@ -14,11 +18,12 @@ d3.json('js/results.json')
 
 function createChart(rawData) {
 
+    console.log(rawData)
     let dataObject = rawData.map((item, i) => {
-        const radius = step * Math.sqrt(i += 0.5);
+        const r = (step + (i * 0.001)) * Math.sqrt(i += 0.5);
         const a = theta * i;
-        const x = width / 2 + radius * Math.cos(a);
-        const y = height / 2 + radius * Math.sin(a);
+        const x = width / 2 + r * Math.cos(a);
+        const y = height / 2 + r * Math.sin(a);
         const parsedDate = new Date(item.date)
 
         let obj = {
@@ -34,6 +39,9 @@ function createChart(rawData) {
             x: x,
             y: y
         }
+        if (!mapRadius.has(r)) {
+            mapRadius.set(r)
+        }
         return obj
     })
 
@@ -43,7 +51,7 @@ function createChart(rawData) {
     const g = svg.append("g")
         .attr("class", "circles");
 
-        // Map for the unique genres: needs value and key for colour scale
+    // Map for the unique genres: needs value and key for colour scale
     let uniqueGenres = new Map()
     let valueKey = 1
     dataObject.map(item => {
@@ -53,47 +61,63 @@ function createChart(rawData) {
         }
     })
 
+    // Scale for the colours
+    // interpolators: https://github.com/d3/d3-scale-chromatic
     let colour = d3.scaleSequential()
         .domain([0, uniqueGenres.size])
-        .interpolator(d3.interpolateViridis)
+        .interpolator(d3.interpolateCool)
 
-    // round diagram
+    // map for the unique years
+    let allYears = d3.group(dataObject, d => d.date.getFullYear())
+    let uniqueYears = Array.from(allYears.keys())
+
+    // scale for years
+    let scaleYears = d3.scaleLinear()
+        .domain(uniqueYears)
+        .range([3, 3.5]);
+
+    // PHYLLOTAXIS DIARGRAM
     let allCircles = g.selectAll("circle")
         .data(dataObject, d => d.id)
         .join("circle")
         .attr("cx", width / 2)
         .attr("cy", height / 2)
         .attr("r", 0)
-        .attr("fill", d => colour(uniqueGenres.get(d.genre)))
+        .attr("fill", 'black')
         .attr('data-tippy-content', (d, i) => {
             return d.title
         })
         .on("click", cardBuilder)
         .on('mouseover', function () {
-            allCircles.transition()
-                .duration(100)
-                .attr("opacity", 0.5)
+            allCircles.attr("opacity", 1).transition()
+                .duration(750)
+                .attr("opacity", 0.2)
             d3.select(this)
                 .transition()
-                .duration(100)
+                .duration(750)
                 .style("cursor", "pointer")
                 .attr("opacity", 1)
-                .attr("r", radius * 2)
+                .attr("r", d => scaleYears(d.date.getFullYear()) * 2)
         })
         .on('mouseout', function () {
             allCircles.transition()
-                .duration(100)
+                .duration(750)
                 .attr("opacity", 1)
             d3.select(this)
                 .transition()
                 .duration(100)
-                .attr("r", radius)
+                .attr("r", d => scaleYears(d.date.getFullYear()))
         })
 
-    allCircles.transition()
-        .delay((d, i) => i)
-        .duration(350)
-        .attr("r", radius)
+    allCircles.attr("opacity", 1)
+        .transition()
+        .ease(d3.easeCircle)
+        .delay((d, i) => i * 2)
+        .duration(750)
+        .attr("fill", d => colour(uniqueGenres.get(d.genre)))
+        // the fill can vary with the index as well
+        // .attr("fill", (d, i) => d3.hsl((step * Math.sqrt(i += 0.5)) % 50, 10, 255))
+        .attr("r", d => scaleYears(d.date.getFullYear()))
         .attr("cx", d => d.x)
         .attr("cy", d => d.y)
 
@@ -118,27 +142,150 @@ function createChart(rawData) {
     }
 
 
-    // graph the Years from here
+    // Legend
+    // ref: https://bl.ocks.org/jkeohan/b8a3a9510036e40d3a4e
 
-    // let uniqueYears = new Set(m.map(d => d.date.getFullYear()))
-    // uniqueYears = Array.from(uniqueYears)
+    const legendC = d3.select('#legend')
+        .append('g')
+        .attr('class', 'legend')
 
-    // let yearScale = d3.scaleBand()
-    //     .domain(uniqueYears)
-    //     .range([height, 0])
+    let dataL = 0;
+    let offset = 50;
 
-    // let yearContainer = svg.append('g')
-    //     .attr('transform', `translate(0, 0)`)
+    let legend = legendC.selectAll('.legend')
+        .data(uniqueGenres)
+        .enter().append('g')
+        .attr("class", "legend")
+        .attr("transform", function (d, i) {
+            if (i === 0) {
+                dataL = (d[0].length * 6) + offset
+                return "translate(0,5)"
+            } else {
+                let newdataL = dataL
+                dataL += (d[0].length * 6) + offset
+                return "translate(" + (newdataL) + ",5)"
+            }
+        })
 
-    // let yearAxis = yearContainer.append('g')
-    //     .call(d3.axisLeft(yearScale))
-    //     // .classed('yearAxis', 'true')
-    //     .attr('class', 'yearAxis')
+    let legendCircle = legend.append('circle')
+        .attr("cx", 5)
+        .attr("cy", 5)
+        .attr("r", 5)
+        .attr("fill", d => colour(d[1]))
 
+    let legendText = legend.append('text')
+        .attr("x", 20)
+        .attr("y", 10)
+        .text(function (d, i) {
+            return d[0]
+        })
+        .attr("class", "textselected")
+        .style("text-anchor", "start")
+        .style("font-size", 13)
+        .style("font-weigth", 200)
+        .style('fill', 'white')
+
+    legendCircle.on('mouseover', function (d, i) {
+
+            let clicked = d3.select(this)
+                .data()[0][0]
+
+            legendCircle.attr("opacity", 1)
+                .transition()
+                .duration(750)
+                .attr("opacity", 0.2)
+
+            legendText.attr("opacity", 1)
+                .transition()
+                .duration(750)
+                .attr("opacity", y => y[0] == clicked ? 1 : 0.2)
+
+            d3.select(this)
+                .transition()
+                .duration(750)
+                .style("cursor", "pointer")
+                .attr("opacity", 1)
+                .attr("r", d => 8)
+        })
+        .on('mouseout', function () {
+            legendCircle.transition()
+                .duration(750)
+                .attr("opacity", 1)
+
+            legendText.transition()
+                .duration(750)
+                .attr("opacity", 1)
+
+            d3.select(this)
+                .transition()
+                .duration(100)
+                .attr("r", d => 5)
+        })
+        .on('click', function (d, i) {
+
+            let clicked = d3.select(this)
+                .data()[0][0]
+
+            legendClick(clicked)
+
+            allCircles.attr("opacity", 1)
+                .transition()
+                .duration(1000)
+                .attr("opacity", y => y.genre == clicked ? 1 : 0.2)
+        })
+
+    function legendClick(catName) {
+        let statsDiv = document.querySelector('#stats')
+        statsDiv.innerHTML = ''
+        statsDiv.style.display = 'block'
+
+        let figure = d3.map(dataObject, d => d.genre).filter(d => d == catName).length
+
+        let category = document.createElement('p')
+        category.setAttribute('class', 'legend-category')
+        category.style.color = colour(uniqueGenres.get(catName))
+        category.innerHTML = `<a class="legend-number">${figure}</a> ${catName} films and series watched`
+        statsDiv.appendChild(category)
+
+        statsDiv.addEventListener('click', function () {
+            this.style.display = 'none'
+            allCircles.attr("opacity", 1)
+                .transition()
+                .duration(1000)
+                .attr("opacity", 1)
+        })
+    }
 }
 
 
-// functions
+
+
+
+
+
+
+
+
+
+// graph the Years from here
+
+// let uniqueYears = new Set(m.map(d => d.date.getFullYear()))
+// uniqueYears = Array.from(uniqueYears)
+
+// let yearScale = d3.scaleBand()
+//     .domain(uniqueYears)
+//     .range([height, 0])
+
+// let yearContainer = svg.append('g')
+//     .attr('transform', `translate(0, 0)`)
+
+// let yearAxis = yearContainer.append('g')
+//     .call(d3.axisLeft(yearScale))
+//     // .classed('yearAxis', 'true')
+//     .attr('class', 'yearAxis')
+
+
+
 
 function cardBuilder(event, d) {
 
